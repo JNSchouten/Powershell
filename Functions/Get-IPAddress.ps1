2f050609590e8721fc9b29494dc31c2a3990b49e
Jacques Schouten <JNSchouten@users.noreply.github.com>
2015-05-25 19:49:17 +0200

    Get IP address from hostname

---
+++ Functions/Get-IPAddress.ps1
@@ -0,0 +1,9 @@
+function Get-IPAddress {
+  param   (
+    [Parameter(ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [String[]]$Name
+  )
+  
+  process
+  { $Name | ForEach-Object { try { [System.Net.DNS]::GetHostByName($_) } catch { } }}
+}
